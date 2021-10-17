bc1640b8b85bafa7e174b5ee4c7cc3c82a6535d5
jbuget <jbuget@gmail.com>
2021-10-17 10:34:28 +0200

    Hide iStock photos sections
    Notice: if we drop the iStock sections, it breaks the navigation (because of some SPA stuff that try to remove the same elements during page change).

--- released_unsplash.js
+++ released_unsplash.js
@@ -1,1 +1,18 @@
-console.log('Unsplash has been free!');
+function dropIStockSections() {
+  const iStockIdentificationPattern = 'Browse premium images on iStock';
+  const headings = document.evaluate(`//h2[contains(., '${iStockIdentificationPattern}')]`, document, null, XPathResult.ANY_TYPE, null);
+
+  const sections = [];
+
+  let heading;
+  while (heading = headings.iterateNext()) {
+    const section = heading.closest('div');
+    sections.push(section);
+  }
+
+  for (const section of sections) {
+    section.style.display = "none";
+  }
+}
+
+dropIStockSections();
